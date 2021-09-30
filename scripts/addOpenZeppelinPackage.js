9a2e09d6b1323318872202bf1a4468a6ec54aba0
Tisamenus <zeroexzerozerodead@protonmail.com>
2021-09-30 20:12:29 +0200

    update script

--- scripts/addOpenZeppelinPackage.js
+++ scripts/addOpenZeppelinPackage.js
@@ -2,7 +2,7 @@ const exec = require('./callFromCommandline.js')
 
 async function addOZPackage() {
   await exec.commandlineCall(
-    `yarn add openzeppelin/${process.argv[2]}/contracts@npm:@openzeppelin/contracts@${process.argv[3]}`,
+    `yarn add @openzeppelin/${process.argv[2]}@npm:@openzeppelin/contracts@${process.argv[3]}`,
   )
 }
 addOZPackage()
